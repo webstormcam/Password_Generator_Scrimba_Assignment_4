b330ed52331ddf5a0ff18f3160a8bce6a80aea80
Cameron Chandler <webstormcam@gmail.com>
2022-12-30 05:38:07 -0500

    Built the basic password maker

--- script.js
+++ script.js
@@ -1,2 +1,16 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
+let output1="";
+let output2="";
+
+function generate(){
+   let password=""
+    for(let i=0;i<15;i++){
+    password+=characters[Math.floor(Math.random()*characters.length)]
+    }
+    return password
+}
+
+output1=generate()
+output2=generate()
+
