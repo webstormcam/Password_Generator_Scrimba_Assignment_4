d5edda6b2fb48ec80886352ea2f55ca4a1c7117b
Cameron Chandler <webstormcam@gmail.com>
2022-12-30 05:55:17 -0500

    Styled the passwords :)

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
+let password1 = document.getElementById("password1")
+let password2 = document.getElementById("password2")
 let output1="";
 let output2="";
 
@@ -10,7 +12,13 @@ function generate(){
     }
     return password
 }
+function createpass(){
+    output1=generate()
+    output2=generate()
 
-output1=generate()
-output2=generate()
+password1.textContent=output1
+password2.textContent=output2
+
+}
+
 
